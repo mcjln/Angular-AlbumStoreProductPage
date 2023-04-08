df6998823bda2e0ebaadcc8c5baf149d2a8b8c96
mcjln <jalalainmarion21@gmail.com>
2023-04-08 04:22:02 -1200

    Done project

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,7 +14,7 @@ import { Product } from './product';
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
-  private _productUrl = '../assets/products.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -22,7 +22,7 @@ export class ProductService {
     return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 
-  getProduct(): Observable<Product[]> {
-    return this._http.get(this._productUrl).map(response => <Product[]>response.json)
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json)
   }
 }
